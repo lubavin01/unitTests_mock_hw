f607b1097ce79de71404bbe19b19f0a14c15a9fb
Nikolay Lyubavin <nik.lubavin@gmail.com>
2020-05-09 19:02:18 +0300

    init

---
+++ src/__tests__/get-level.test.js
@@ -0,0 +1,32 @@
+import {getLevel} from '../get-level';
+import fetchData from '../http';
+
+jest.mock('../http');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('getLevel', () => {  
+
+  test('get level 1', () => {
+
+    fetchData.mockReturnValue({status: 'ok', level: '1'});
+
+    const result = getLevel(1);
+
+    expect(result).toBe(`Ваш текущий уровень: 1`);
+
+  });
+
+  test('get level - error', () => {
+
+    fetchData.mockReturnValue({status: 'error'});
+
+    const result = getLevel();
+
+    expect(result).toBe(`Информация об уровне временно недоступна`);
+
+  });
+
+});
